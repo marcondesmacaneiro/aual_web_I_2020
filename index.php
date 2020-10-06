5a240250de18518d63188db1d39ca59dcb47b0ac
Marcondes Maçaneiro <marcondesmacaneiro@gmail.com>
2020-10-06 22:58:35 +0000

    imp

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    <h1><?php echo "Página do Marcondes"; ?></h1>
+    <h1><?="Página do Marcondes"?></h1>
     <?php //depende de configuração no php.ini ShortOpenTag => <?
         echo 'Marcondes Maçaneiro'
     ?>
