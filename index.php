a294d3cfc61cff78b8a62ee4002fd95c114e0cf2
Marcondes Maçaneiro <marcondesmacaneiro@gmail.com>
2020-10-07 00:14:41 +0000

    ok

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+    ini_set('display_errors', 1); 
+    ini_set('display_startup_errors', 1); 
+    error_reporting(E_ALL);
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,11 +13,46 @@
 </head>
 <body>
     <?php
-        $nome = "Marcondes Maçaneiro";
+        define("SENHA_DATABASE","123456");
+
+        include "titulo.php";
+
+        if (true)
+            echo "ok";
+
+        (true)?"ok":"falsa";
+
+        $nomes["nome"] = "Marcondes";
+        $nomes["nome_filha"] = "Leticia";
+        $nomes["sobrenome"] = "Maçaneiro";
+        $nomes["cidade"] = "Rio do Sul";
+
+        echo "<br/>";
+        var_dump($nomes);
+        echo "<br/>";
+
+        $keys = array_keys($nomes);
+        echo "<br/>";
+        var_dump($keys);
+        echo "<br/>";
+
+        for ($i=0; $i < count($keys); $i++) { 
+            echo "O valor é: {$nomes[$keys[$i]]}<br/>";
+        }
     ?>
-    <h1><?="Página do ".$nome?></h1>
+
+    <hr />
+        <?php
+            print_r($_GET);
+
+            include "{$_GET['pagina']}.php";
+        ?>
+    <hr />
+
     <?php //depende de configuração no php.ini ShortOpenTag => <?
-        echo 'Marcondes Maçaneiro'
+        echo 'Marcondes Maçaneiro';
+        $object = $_SERVER;
+        error_log( print_r( $object, true ) );
     ?>
 </body>
 </html>
---
+++ titulo.php
@@ -0,0 +1,4 @@
+    <?php
+        $nome = "Marcondes Maçaneiro....";
+    ?>
+    <h1><?="Página do {$nome}"?></h1>
